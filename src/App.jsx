a27f9b36a1bd4a4960e348fe8173bfee72489729
MobbySchiller <kb.burzynski@gmail.com>
2022-12-05 20:38:59 +0100

    Add Header, SlideBar and Team

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from 'react-router-dom'
 import TopBar from './scenes/global/TopBar'
 import SideBar from './scenes/global/SideBar'
 import Dashboard from './scenes/dashboard/Dashboard'
-// import Team from './scenes/team/Team'
+import Team from './scenes/team/Team'
 // import Invoices from './scenes/global/Invoices'
 // import Contacts from './scenes/global/Contacts'
 // import Bar from './scenes/global/Bar'
@@ -29,8 +29,8 @@ const App = () => {
             <TopBar />
             <Routes>
               <Route path='/' element={<Dashboard />} />
-              {/* <Route path='/team' element={<Team />} />
-              <Route path='/contacts' element={<Contacts />} />
+              <Route path='/team' element={<Team />} />
+              {/* <Route path='/contacts' element={<Contacts />} />
               <Route path='/invoices' element={<Invoices />} />
               <Route path='/form' element={<Form />} />
               <Route path='/bar' element={<Bar />} />
--- src/theme.js
+++ src/theme.js
@@ -21,7 +21,7 @@ export const tokens = (mode) => ({
                 100: "#d0d1d5",
                 200: "#a1a4ab",
                 300: "#727681",
-                400: "#434957",
+                400: "#1F2A40",
                 500: "#141b2d",
                 600: "#101624",
                 700: "#0c101b",
@@ -162,7 +162,7 @@ export const themeSettings = (mode) => {
         },
         typography: {
             fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
-            fontSize: 40,
+            fontSize: 12,
             h1: {
                 fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
                 fontSize: 40,
