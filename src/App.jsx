eb86dba795edd6142af1123116cbc7c1c539c777
MobbySchiller <kb.burzynski@gmail.com>
2022-12-05 17:16:54 +0100

    Add theme settings, routing and TopBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { ColorModeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { Routes, Route } from 'react-router-dom'
+import TopBar from './scenes/global/TopBar'
+import SideBar from './scenes/global/SideBar'
+import Dashboard from './scenes/dashboard/Dashboard'
+// import Team from './scenes/team/Team'
+// import Invoices from './scenes/global/Invoices'
+// import Contacts from './scenes/global/Contacts'
+// import Bar from './scenes/global/Bar'
+// import Form from './scenes/global/Form'
+// import Line from './scenes/global/Line'
+// import Pie from './scenes/global/Pie'
+// import FAQ from './scenes/global/FAQ'
+// import Geography from './scenes/global/Geography'
+// import Calendar from './scenes/global/Calendar'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const [theme, colorMode] = useMode()
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className='app'>
+          <SideBar />
+          <main className='content'>
+            <TopBar />
+            <Routes>
+              <Route path='/' element={<Dashboard />} />
+              {/* <Route path='/team' element={<Team />} />
+              <Route path='/contacts' element={<Contacts />} />
+              <Route path='/invoices' element={<Invoices />} />
+              <Route path='/form' element={<Form />} />
+              <Route path='/bar' element={<Bar />} />
+              <Route path='/pie' element={<Pie />} />
+              <Route path='/line' element={<Line />} />
+              <Route path='/faq' element={<FAQ />} />
+              <Route path='/geography' element={<Geography />} />
+              <Route path='/calendar' element={<Calendar />} /> */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
